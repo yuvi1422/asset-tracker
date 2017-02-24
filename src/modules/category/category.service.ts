9e7b629ff50bdb584c448c1c89d354556872ab31
Yuvraj Patil <yuvraj.patil.it@gmail.com>
2017-02-24 16:02:43 +0530

    Changed path of category.json for android devices

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -2,23 +2,30 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { Platform } from 'ionic-angular';
+
 @Injectable()
 export class CategoryService {
 
-  constructor(private http: Http) {}
+  constructor(private http: Http, private platform: Platform ) { }
 
-   /**
-   * @description Function to get categories 
-   * @return {Array} Promise with List of categories
-   */
+  /**
+  * @description Function to get categories 
+  * @return {Array} Promise with List of categories
+  */
   getCategories() {
-    let url = 'data/category.json';
+
+    var url = 'assets/data/category.json';
+
+    if (this.platform.is('cordova') && this.platform.is('android')) {
+      url = "/android_asset/www/" + url;
+    }
 
     return this.http.get(url)
       .map((res) => {
         return res.json()
       });
 
-   }
+  }
 
 }
