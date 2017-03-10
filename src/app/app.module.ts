ba3a72217babff7eb1bcef7a1c6a1650032b60a7
Yuvraj Patil <yuvraj.patil.it@gmail.com>
2017-03-10 13:47:32 +0530

    Initialized repo with ionic blank
    Scraped previous codebase due to it's design flaws. Creating a fresh setup with ionic blank template.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
-import { Modal } from '../modules/modal/modal';
+import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2,
-    Modal
+    HomePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -18,9 +14,7 @@ import { Modal } from '../modules/modal/modal';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2,
-    Modal
+    HomePage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
