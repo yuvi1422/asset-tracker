14e8a828be066742d40aa25bebd6d77b0a721415
Yuvraj Patil <yuvraj.patil.it@gmail.com>
2017-05-03 12:08:10 +0530

    Added Comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,19 @@ import { HomeComponent } from '../pages/home/home.component';
   templateUrl: 'app.html'
 })
 export class MyApp {
+
+   /**
+   * @description Root Page of Application
+   */
   rootPage = HomeComponent;
 
+  /**
+  * @constructor 
+  * @param platform Platform service of ionic
+  */
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+      StatusBar.styleDefault(); // Okay, so the platform is ready and our plugins are available.  Here you can do any higher level native things you might need.
       Splashscreen.hide();
     });
   }
